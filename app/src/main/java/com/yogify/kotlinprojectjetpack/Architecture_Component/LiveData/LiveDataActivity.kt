e51140fe0891c1bbdd87554a3242ccfd5de2263b
RohitashYogi <rohitash.yogi@probusinsurance.com>
2022-01-21 18:11:35 +0530

    Data Binding Is Working

--- app/src/main/java/com/yogify/kotlinprojectjetpack/Architecture_Component/LiveData/LiveDataActivity.kt
+++ app/src/main/java/com/yogify/kotlinprojectjetpack/Architecture_Component/LiveData/LiveDataActivity.kt
@@ -1,14 +1,11 @@
 package com.yogify.kotlinprojectjetpack.Architecture_Component.LiveData
 
-import android.database.DatabaseUtils
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.get
-import com.yogify.kotlinprojectjetpack.Architecture_Component.QuotesApp.ViewModule.MainViewModule
+import com.yogify.kotlinprojectjetpack.NamedobData
 import com.yogify.kotlinprojectjetpack.R
 import com.yogify.kotlinprojectjetpack.databinding.ActivityLiveDataBinding
 
@@ -28,7 +25,7 @@ class LiveDataActivity : AppCompatActivity() {
             mainViewModule.updatelivedata("Aditya")
         }
 
-        // Data binding concept is used to bind data into view on xml it self
+        // Data binding Concept is used to bind data into view on xml it self
 
         var singledata = NamedobData("Rohitash yogi", "04-07-1998")
         binding.datauser = singledata
